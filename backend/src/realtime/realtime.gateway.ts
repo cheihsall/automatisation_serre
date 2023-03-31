c1c77daad6275d9f7e40806eb6a9184b86b87ea2
Josephine <sosodjibrildiop@gmail.com>
2023-03-31 08:40:24 +0000

    comm

--- backend/src/realtime/realtime.gateway.ts
+++ backend/src/realtime/realtime.gateway.ts
@@ -12,9 +12,44 @@ import {
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
+  
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 import { Server } from 'ws';
+
+/* import { SerialPort } from 'serialport';
+const {Readline } = require('@serialport/parser-readline')
+export const RealtimeService= () => {
+  const port = new SerialPort(
+ 
+    {
+      path: '/dev/ttyACM0', baudRate: 9600
+    },
+    (err) => {
+      if (err) {
+        console.error(err)
+        
+        
+      }
+      console.log('success')
+    },
+  );
+  
+  const parser = new Readline({ delimiter: '\r\n' });
+  
+  port.pipe(parser);
+
+ 
+  parser.on('data', (data) => {
+    console.log(data);
+   
+  })
+  
+} */
+  
+/* const SerialPort = require('serialport'); */
+
+
 
 @WebSocketGateway({ cors: true })
 export class RealtimeGateway
@@ -24,11 +59,15 @@ export class RealtimeGateway
   @WebSocketServer()
   server: Server;
 
+  
+
   handleDisconnect(client: any) {
     throw new Error('Method not implemented.');
   }
   handleConnection(@ConnectedSocket() client: Socket) {
+    console.log('Connexion Websocket') 
     setInterval(() => {
+      
       client.emit('parametre', this.data);
     }, 1000);
     client.on('systeme', (data: any) => {
